01722ccb4342bc20ed890d570de7ffac144ad500
Martin Rubinsztein <martin.rubinsztein@gmail.com>
2021-07-09 09:47:39 +1000

    No using store

--- packages/web-stories/src/playground-bundle.js
+++ packages/web-stories/src/playground-bundle.js
@@ -1,39 +1,27 @@
 import { define as defineIframe } from '@nytramr/nr-iframe';
 import { define as defineTabs } from '@nytramr/nr-tabs';
 import { EventManager } from './playground/event-listener';
-import { store, getEvents } from './playground/store';
 
 defineTabs();
 defineIframe();
 
 const eventManager = new EventManager();
 
-// document.addEventListener('load-content', (event) => console.log('load', event, event.target));
-document.addEventListener('DOMContentLoaded', (event) => {
-  console.log('DOMContentLoaded', event);
-
-  const iFrames = document.querySelectorAll('nr-iframe-container');
-
-  Array.from(iFrames).forEach((iFrame) => {
-    iFrame.addEventListener('load-content', (event) => {
-      console.log('load content', event, event.target);
-      const Bridge = event.target.Bridge;
-      console.log(Bridge.version);
-      if (Bridge) {
-        const instance = new Bridge('makeIdUnique', { eventManager });
-        instance.init({ events: [{ eventName: 'click' }] });
-      } else {
-        console.log('no bridge');
-      }
-    });
-
-    // if (Bridge) {
-    //   const instance = new Bridge();
-    //   instance.init();
-    // } else {
-    //   console.log('no bridge');
-    // }
-  });
+eventManager.addSubscriber((payload) => {
+  console.log('emitter', payload);
 });
 
-store.subscribe(console.log, getEvents);
+const iFrames = document.querySelectorAll('nr-iframe-container');
+
+Array.from(iFrames).forEach((iFrame) => {
+  iFrame.addEventListener('load-content', (event) => {
+    const Bridge = event.target.Bridge;
+    console.log(Bridge.version);
+    if (Bridge) {
+      const instance = new Bridge('makeIdUnique', { eventManager });
+      instance.init({ events: [{ eventName: 'click' }] });
+    } else {
+      console.log('no bridge');
+    }
+  });
+});
--- packages/web-stories/src/playground/event-listener.js
+++ packages/web-stories/src/playground/event-listener.js
@@ -1,8 +1,24 @@
-import { printEventActionDispatcher } from './store';
+import { Emitter } from '@nytramr/utils/lib/emitter';
 
-export class EventManager {
-  printEvent(eventName, event) {
-    console.log('event manager', eventName, event);
-    printEventActionDispatcher(eventName, event);
+const eventLog = Symbol('event-log');
+const eventLogTemplate = Symbol('event-log-template');
+export class EventManager extends Emitter {
+  constructor() {
+    super(); // always initialize super
+    // get the div where I will add the events
+    this[eventLog] = document.getElementById('event-log');
+    this[eventLogTemplate] = document.getElementById('event-log-template');
+  }
+  printEvent(name, event) {
+    const eventElement = this[eventLogTemplate].content.cloneNode(true);
+    const eventName = eventElement.querySelector('.event-name');
+    const eventData = eventElement.querySelector('.event-data');
+
+    eventName.textContent = name;
+    eventData.textContent = JSON.stringify(Object.assign({}, event));
+    this[eventLog].prepend(eventElement);
+
+    // Maybe there is no need to emit anything.
+    this.emit({ eventName: name, event });
   }
 }
